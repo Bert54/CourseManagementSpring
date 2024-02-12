4d51b5b13b74053eba82cb4864641942e23dbf14
Matthias <matt.bert@live.fr>
2024-02-12 20:39:05 +0100

    Init repo

---
+++ src/main/java/com/test/coursemanagementspring/CourseManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.test.coursemanagementspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManagementSpringApplication.class, args);
+    }
+
+}
